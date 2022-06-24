881901aea756a7cd353f4a481104cdc750435d55
Toktorov <ktoktorov2005@gmail.com>
2022-06-24 16:45:51 +0600

    config project, add apps and models

---
+++ apps/settings/models.py
@@ -0,0 +1,18 @@
+from tabnanny import verbose
+from django.db import models
+
+# Create your models here.
+class Setting(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    logo = models.ImageField(upload_to = 'logo/')
+    phone = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    email = models.EmailField(max_length=150)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Настройка"
+        verbose_name_plural = "Настройки"
