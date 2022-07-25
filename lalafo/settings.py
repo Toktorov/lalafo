9e005f34d4cd42eab2130aa393ddddec22243f46
Toktorov <ktoktorov2005@gmail.com>
2022-07-25 17:18:29 +0600

    heroku ready

--- lalafo/settings.py
+++ lalafo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -173,3 +174,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
+
+django_heroku.settings(locals())
