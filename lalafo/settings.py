caf991d00a8d70003f86547cc7b3293dcbed9661
Toktorov <ktoktorov2005@gmail.com>
2022-07-27 16:36:06 +0600

    ready to deploy

--- lalafo/settings.py
+++ lalafo/settings.py
@@ -120,8 +120,12 @@ WSGI_APPLICATION = 'lalafo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lalafo',
+        'USER': 'back',
+        'PASSWORD': '2020itrun',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
